144e1614f7215aa411382b721835dc7c767c2c02
halls-a <joon28412@gmail.com>
2025-03-12 16:37:31 +0900

    asd

--- Project1/FileName.c
+++ Project1/FileName.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-int main(void) {
-	printf("Hello World");
-	return 0;
+int main() {
+		printf("FirstApp 프로그램\n");
+		printf("작성자: 전기준\n");
+		printf("버전 1.0\n");
+
+		return 0;
 }
